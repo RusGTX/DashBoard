dd195511b207f00caa27d99ea690fb8ca77c86ae
RusGTX <109609116+RusGTX@users.noreply.github.com>
2023-07-30 23:01:32 +0400

    1st commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const orders = [
+    {
+        productName:'Foldable mini dron',
+        productnumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName:'Lavendale KF102 dron',
+        productnumber: '36821',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName:'Roocko F1 dron',
+        productnumber: '49326',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName:'GPT 4KU dron',
+        productnumber: '28331',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName:'Lorenzo KUO dron',
+        productnumber: '21563',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
